01bb4ea3acd2c573619a486ae437332713b24e0c
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2023-05-17 17:59:54 +0300

    dumpy forget

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -3,9 +3,9 @@ import MainInfo from "../components/user-page/Co-op Containers/MainInfo";
 import "../styles/user-page/User-Home.css";
 import Opportunities from "../components/user-page/Co-op Containers/Opportunities";
 import Operations from "../components/user-page/Co-op Containers/Operations";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
 
 const UserPage = function () {
   // const id = localStorage.getItem("id");
