f243c6bd44c5b597ed0a29fe86ad684dfd16d2df
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2023-05-31 19:42:41 +0300

    route fix test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import UserPage from './pages/UserPage';
 import TechAbout from './components/tech-about/TechAbout';
@@ -7,14 +7,12 @@ import About from "./components/home-page/about/About";
 function App() {
   return (
     <div className="body">
-      <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path = '/account' element = {<UserPage/>} /> 
           <Route path="/tech-about" element={<TechAbout/>} />
           <Route path="/about" element={<About/>} />
         </Routes>
-      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <GoogleOAuthProvider clientId='76117731491-v6vmn6qs6m1f2ahl4elukmcuhkoojd1p.apps.googleusercontent.com'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </GoogleOAuthProvider>,
+  </GoogleOAuthProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
