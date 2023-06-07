e34ad55edc0136d17b6021796598f0896d1bbf6c
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2023-06-07 22:45:04 +0300

    removed currency info request

--- src/components/user-page/Main Components/Balance.js
+++ src/components/user-page/Main Components/Balance.js
@@ -4,8 +4,6 @@ import axios from "axios";
 
 const Balance = function (props) {
   const [balance, setBalance] = useState(props.cardInfo.card_balance);
-  const [currencyUSD, setCurrencyUSD] = useState([]);
-  const [currencyEUR, setCurrencyEUR] = useState([]);
 
   const fetchBalance = async () => {
     const id = localStorage.getItem("id");
@@ -16,59 +14,9 @@ const Balance = function (props) {
       const data = response.data;
       setBalance(data.card_balance.toFixed(2));
     } catch (error) {
-      console.error(error); //
+      console.error(error);
     }
   };
-
-  const getCurrencyInfo = function () {
-    axios
-      .get("https://api.monobank.ua/bank/currency")
-      .then((response) => setCurrencyUSD(response.data[0]))
-      .catch((error) => console.log(error));
-    axios
-      .get("https://api.monobank.ua/bank/currency")
-      .then((response) => setCurrencyEUR(response.data[1]))
-      .catch((error) => console.log(error));
-  };
-
-
-
-  const updateCurrenciesInfo = function () {
-    // update USD info
-    axios
-      .post("https://mono-lite-back.azurewebsites.net/currency", {
-        currency_id: currencyUSD.currencyCodeA,
-        date: currencyUSD.date,
-        rateSell: currencyUSD.rateSell,
-        rateBuy: currencyUSD.rateBuy,
-      })
-      .then((response) => response && "")
-      .catch((error) => console.log(error));
-
-    // Update Euro info
-    axios
-      .post("https://mono-lite-back.azurewebsites.net/currency", {
-        currency_id: currencyEUR.currencyCodeA,
-        date: currencyEUR.date,
-        rateSell: currencyEUR.rateSell,
-        rateBuy: currencyEUR.rateBuy,
-      })
-      .then((response) => response && "")
-      .catch((error) => console.log(error));
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      getCurrencyInfo();
-      updateCurrenciesInfo();
-    }, 600000 * 60); // Виконувати кожні 5 хвилин
-    // ...
-    return () => {
-      clearInterval(intervalId);
-    };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   useEffect(() => {
     //КОСТИЛЬ ? (Of course)
